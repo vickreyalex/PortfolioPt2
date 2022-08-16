06e1af3c823d50137897ce095780ca3e07a74577
vickreyalex <111119944+vickreyalex@users.noreply.github.com>
2022-08-16 01:54:37 -0700

    Update index.jsx
    updated contact info footer

--- src/Layout/Footer/index.jsx
+++ src/Layout/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Index() {
     return (
         <div className='max-w-7xl mx-auto py-2'>
-            <p>&copy; All right reserved</p>
+            <p>&copy; vickreyalex@gmail.com </p>
         </div>
     )
 }
