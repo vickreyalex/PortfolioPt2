39761a8f88a4ee57d8c70c615ec64d84961db38a
vickreyalex <111119944+vickreyalex@users.noreply.github.com>
2022-08-16 01:56:04 -0700

    Update index.jsx
    removed copyright symbol

--- src/Layout/Footer/index.jsx
+++ src/Layout/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Index() {
     return (
         <div className='max-w-7xl mx-auto py-2'>
-            <p>&copy; vickreyalex@gmail.com </p>
+            <p>vickreyalex@gmail.com </p>
         </div>
     )
 }
